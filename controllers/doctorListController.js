25dbb767cd0881b874da749f14d02a57a0c21f20
unknown <Ranjota Ballal>
2024-11-16 23:11:01 -0500

    Doctor list search

--- controllers/doctorListController.js
+++ controllers/doctorListController.js
@@ -10,4 +10,26 @@ const getDoctors = async (req, res) => {
     }
 };
 
-module.exports = getDoctors;
+const searchDoctors = async (req, res) => {
+   const {query} = req.query;
+
+   try {
+        const doctors = await DoctorsList.find({
+            $or: [
+                {name: {$regex: query, $options: 'i'}}, //Case insensitive name
+                {specialty: {$regex: query, $options: 'i'}}, //Case insensitive specialty
+            ]
+        });  
+        
+        res.status(200).json(doctors);
+   } catch(error) {
+        console.error('Error during search:', error);
+        res.status(500).json({message:'Failed to search doctors'});
+   }
+};
+
+
+module.exports = {
+    getDoctors,
+    searchDoctors
+};
--- routes/doctorListRoutes.js
+++ routes/doctorListRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const getDoctors = require('../controllers/doctorListController');
+const {getDoctors, searchDoctors} = require('../controllers/doctorListController');
+
 
 router.get('/', authMiddleware, getDoctors);
+
+router.get('/search', authMiddleware, searchDoctors);
 
 module.exports = router;
