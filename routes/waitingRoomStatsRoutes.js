2e003996b5142a63032914e70179712daca1497d
unknown <Ranjota Ballal>
2024-12-18 01:38:56 -0500

    setInitialStats bug fix in routes

--- routes/waitingRoomStatsRoutes.js
+++ routes/waitingRoomStatsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { cancelAppointment, updateWaitingRoomDetails, getLiveQueueDetails } = require('../controllers/waitingRoomStatsController');
+const { getWaitingRoomDetails, cancelAppointment, updateWaitingRoomDetails, getLiveQueueDetails } = require('../controllers/waitingRoomStatsController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', authMiddleware, getWaitingRoomDetails );
