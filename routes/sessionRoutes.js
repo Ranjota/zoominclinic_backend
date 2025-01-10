3b9175da670d14cc3307c038367d4fa7dbcada68
unknown <Ranjota Ballal>
2025-01-10 01:20:24 -0500

    Updating route name for assigning doctors

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { assignDoctorToPatient } = require('../controllers/sessionController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('/', authMiddleware, assignDoctorToPatient);
+router.get('/assign-doctor-by-patient', authMiddleware, assignDoctorToPatient);
 
 module.exports = router;
