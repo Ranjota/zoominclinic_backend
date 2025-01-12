e6e551144b21ef1dc7afcb5dff24f68c35408e30
unknown <Ranjota Ballal>
2025-01-11 22:56:42 -0500

    Bug fix for session route

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { assignDoctorToPatient } = require('../controllers/sessionController');
+const { assignDoctorToPatient, acceptSession } = require('../controllers/sessionController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/assign-doctor-by-patient', authMiddleware, assignDoctorToPatient);
